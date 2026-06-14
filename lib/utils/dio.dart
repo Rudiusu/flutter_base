7dc3832cc43352b420334ed1fb9c40d784970e8e
rudiusu <1163485800@qq.com>
2026-06-14 20:57:53 +0800

    util:dio工具类添加

---
+++ lib/utils/dio.dart
@@ -0,0 +1,47 @@
+import 'package:dio/dio.dart';
+
+class BaseResp<T> {
+  final int code;
+  final String message;
+  final T? data;
+
+  BaseResp({required this.code, required this.message, this.data});
+
+  bool get isSuccess => code == 200;
+}
+
+class DioUtils {
+  final Dio _dio = Dio();
+  DioUtils() {
+    _dio.options
+      ..baseUrl = ""
+      ..connectTimeout = Duration(seconds: 10)
+      ..sendTimeout = Duration(seconds: 10)
+      ..receiveTimeout = Duration(seconds: 10);
+
+    _addInterceptor();
+  }
+  void _addInterceptor() {
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          handler.next(options);
+        },
+        onResponse: (response, handler) {
+          if (response.statusCode! >= 200 && response.statusCode! < 300) {
+            handler.next(response);
+            return;
+          }
+          handler.reject(DioException(requestOptions: response.requestOptions));
+        },
+        onError: (error, handler) {
+          handler.reject(error);
+        },
+      ),
+    );
+  }
+
+  Future<Response<BaseResp>> get(String path, {Map<String, dynamic>? params}) {
+    return _dio.get(path, queryParameters: params);
+  }
+}
